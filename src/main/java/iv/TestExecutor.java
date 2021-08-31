ca64d04445505843aa8a50a40638467ca5fb4446
Yoshiki Higo <yoshiki.higo@gmail.com>
2021-08-31 17:59:58 +0900

    add function to classify methods based on their behavior

--- src/main/java/iv/TestExecutor.java
+++ src/main/java/iv/TestExecutor.java
@@ -7,6 +7,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -66,14 +67,14 @@ public class TestExecutor {
     try {
       // 各カテゴリのディレクトリを得る
       final List<Path> groupDirs = Files.list(outputPath)
-          .filter(p -> Files.isDirectory(p))
+          .filter(Files::isDirectory)
           .collect(Collectors.toList());
 
       // 各カテゴリに対して処理を行う
       // ここでの処理とは，切り出したメソッドのコンパイルとテスト生成
       for (final Path groupDir : groupDirs) {
         final List<Path> targetDirs = Files.list(groupDir)
-            .filter(p -> Files.isDirectory(p))
+            .filter(Files::isDirectory)
             .collect(Collectors.toList());
 
         final Map<Path, Path> sourceTestMap = new HashMap<>();
@@ -81,10 +82,10 @@ public class TestExecutor {
         // 各対象のクラス（メソッド）に対して処理を行う
         for (final Path targetDir : targetDirs) {
 
-          final List<String> javaVersionCommand = new ArrayList<>();
-          javaVersionCommand.add("java");
-          javaVersionCommand.add("-version");
-          executeProcess(javaVersionCommand);
+          // "_test"で終わる場合はテストディレクトリだからスキップ
+          if(targetDir.toString().endsWith("_test")){
+            continue;
+          }
 
           // 対象のメソッドをコンパイル
           final List<String> javacCommand = new ArrayList<>();
@@ -100,44 +101,93 @@ public class TestExecutor {
 
           // 対象のメソッドのテストを生成
           final Path testDir = Paths.get(targetDir + "_test");
-          final List<String> evosuiteCommand = new ArrayList<>();
-          evosuiteCommand.add("java");
-          evosuiteCommand.add("-jar");
-          evosuiteCommand.add("lib/evosuite-1.1.0.jar");
-          evosuiteCommand.add("-class");
-          evosuiteCommand.add("Target");
-          evosuiteCommand.add("-projectCP");
-          evosuiteCommand.add(targetDir.toString());
-          evosuiteCommand.add("-Dtest_dir=" + testDir);
-          final int evosuiteCommandExitValue = executeProcess(evosuiteCommand);
+          if(Files.notExists(testDir)) { // テストディレクトリが既に存在する場合は処理をスキップ
+            final List<String> evosuiteCommand = new ArrayList<>();
+            evosuiteCommand.add("java");
+            evosuiteCommand.add("-jar");
+            evosuiteCommand.add("lib/evosuite-1.1.0.jar");
+            evosuiteCommand.add("-class");
+            evosuiteCommand.add("Target");
+            evosuiteCommand.add("-projectCP");
+            evosuiteCommand.add(targetDir.toString());
+            evosuiteCommand.add("-Dtest_dir=" + testDir);
+            final int evosuiteCommandExitValue = executeProcess(evosuiteCommand);
 
-          // evosuiteによるテスト生成が失敗した場合には以降の処理をしない
-          if (evosuiteCommandExitValue != 0) {
+            // evosuiteによるテスト生成が失敗した場合には以降の処理をしない
+            if (evosuiteCommandExitValue != 0) {
+              continue;
+            }
+          }
+
+          // 生成したテストをコンパイル
+          final List<String> javacCommand2 = new ArrayList<>();
+          javacCommand2.add("javac");
+          //javacCommand2.add(testDir.resolve("*.java").toString());
+          javacCommand2.add(testDir.resolve("Target_ESTest.java").toString());
+          javacCommand2.add(testDir.resolve("Target_ESTest_scaffolding.java").toString());
+          final String cp = targetDir + ":" +
+              testDir.toString() + ":" +
+              "lib/evosuite-standalone-runtime-1.1.0.jar:" +
+              "lib/junit-4.13.2.jar:" +
+              "lib/hamcrest-core-1.3.jar";
+          System.out.println("CLASSPATH=" + cp);
+          final Map<String, String > environmentVariables1 = new HashMap<>();
+          environmentVariables1.put("CLASSPATH", cp);
+          final int javaCommandExitValue2 = executeProcess(javacCommand2, environmentVariables1);
+
+          // コンパイルに失敗した場合には以降の処理をしない
+          if(javaCommandExitValue2 != 0){
             continue;
           }
+
+
 
           sourceTestMap.put(targetDir, testDir);
         }
 
-        final Entry<Path, Path>[] entries = sourceTestMap.entrySet().toArray(Entry[]::new);
-        for(int left = 0 ; left < entries.length ; left++){
-          for(int right = left+1 ; right < entries.length ; right++){
+        final Entry<Path, Path>[] entries = sourceTestMap.entrySet()
+            .toArray(Entry[]::new);
+        for (int left = 0; left < entries.length; left++) {
+          for (int right = left + 1; right < entries.length; right++) {
 
-            if(left == right){
+            if (left == right) {
               continue;
             }
 
             final List<String> junit1command = new ArrayList<>();
             junit1command.add("java");
             junit1command.add("org.junit.runner.JUnitCore");
-            junit1command.add("Target");
+            junit1command.add("Target_ESTest");
+
+            // leftのソースがrightのテストをパスするかを確認
+            final String cp1 = entries[left].getKey() + ":" + entries[right].getValue() + ":" +
+                "lib/evosuite-standalone-runtime-1.1.0.jar:" +
+            "lib/junit-4.13.2.jar:" +
+            "lib/hamcrest-core-1.3.jar";
+            System.out.println(cp1);
+            final Map<String, String > environmentVariables1 = new HashMap<>();
+            environmentVariables1.put("CLASSPATH", cp1);
+            final int junit1CommandExitValue = executeProcess(junit1command, environmentVariables1);
+            if(junit1CommandExitValue != 0){
+              continue;
+            }
+
+            // rightのソースがleftのテストをパスするかを確認
+            final String cp2 = entries[right].getKey() + ":" + entries[left].getValue() + ":" +
+                "lib/evosuite-standalone-runtime-1.1.0.jar:" +
+                "lib/junit-4.13.2.jar:" +
+                "lib/hamcrest-core-1.3.jar";
+            System.out.println(cp2);
+            final Map<String, String > environmentVariables2 = new HashMap<>();
+            environmentVariables2.put("CLASSPATH", cp2);
+            final int junit2CommandExitValue = executeProcess(junit1command, environmentVariables2);
+            if(junit2CommandExitValue != 0){
+              continue;
+            }
+
+            System.out.println(entries[left].getValue() + " : " + entries[right].getValue());
           }
         }
-
-        sourceTestMap.entrySet()
-            .forEach(e -> System.out.println(e.getKey()
-                .toString() + " -> " + e.getValue()
-                .toString()));
 
       }
 
@@ -149,13 +199,18 @@ public class TestExecutor {
     }
   }
 
-  private int executeProcess(final List<String> command) {
+  private int executeProcess(final List<String> command){
+    return executeProcess(command, Collections.EMPTY_MAP);
+  }
+
+  private int executeProcess(final List<String> command, final Map<String, String> environmentVariables) {
     try {
       final ProcessBuilder processBuilder = new ProcessBuilder(command);
       processBuilder.redirectErrorStream(true);
       final Map<String, String> environment = processBuilder.environment();
       environment.put("JAVA_HOME",
           "/Library/Java/JavaVirtualMachines/jdk-15.0.2.jdk/Contents/Home");
+      environment.putAll(environmentVariables);
       final Process javacProcess = processBuilder.start();
       final StreamThread outThread = new StreamThread(javacProcess.getInputStream(), System.out);
       outThread.start();
--- src/main/java/iv/data/JavaMethod.java
+++ src/main/java/iv/data/JavaMethod.java
@@ -53,7 +53,7 @@ public class JavaMethod {
     lines.add("    // lines: " + startLine + " to " + endLine);
     lines.add("    // permalink: " + getPermalink());
     for (final String line : text.split(System.lineSeparator())) {
-      lines.add("    " + line);
+      lines.add("    " + line.replace(name + "(", "__target__("));
     }
 
     lines.add("");
