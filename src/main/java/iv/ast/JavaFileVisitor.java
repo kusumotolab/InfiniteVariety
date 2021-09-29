1def95b2b28870431be694a49bb470c43da3127b
Yoshiki Higo <yoshiki.higo@gmail.com>
2021-09-29 15:51:59 +0900

    java.lang and java.util are added as target types

--- src/main/java/iv/ast/JavaFileVisitor.java
+++ src/main/java/iv/ast/JavaFileVisitor.java
@@ -1,9 +1,11 @@
 package iv.ast;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Stream;
 import org.eclipse.jdt.core.dom.ASTNode;
 import org.eclipse.jdt.core.dom.ASTVisitor;
 import org.eclipse.jdt.core.dom.ArrayType;
@@ -34,6 +36,220 @@ public class JavaFileVisitor extends ASTVisitor {
   private final RevCommit commit;
   private final String path;
   private final List<JavaMethod> javaMethods;
+  private final String[] javalangClasses = new String[] {
+      "AbstractMethodError",//
+      "Appendable",//
+      " ArithmeticException",//
+      "ArrayIndexOutOfBoundsException",//
+      "ArrayStoreException",//
+      "AssertionError",//
+      "AutoCloseable",//
+      "Boolean",//
+      "BootstrapMethodError",//
+      "Byte",//
+      "Character",//
+      "CharSequence",//
+      "Class",//
+      "ClassCastException",//
+      "ClassCircularityError",//
+      "ClassFormatError",//
+      "ClassLoader",//
+      "ClassNotFoundException",//
+      "ClassValue",//
+      "Cloneable",//
+      "CloneNotSupportedException",//
+      "Comparable",//
+      "Compiler",//
+      "Deprecated",//
+      "Double",//
+      "Enum",//
+      "EnumConstantNotPresentException",//
+      "Error",//
+      "Exception",//
+      "ExceptionInInitializerError",//
+      "Float",//
+      "FunctionalInterface",//
+      "IllegalAccessError",//
+      "IllegalAccessException",//
+      "IllegalArgumentException",//
+      "IllegalCallerException",//
+      "IllegalMonitorStateException",//
+      "IllegalStateException",//
+      "IllegalThreadStateException",//
+      "IncompatibleClassChangeError",//
+      "IndexOutOfBoundsException",//
+      "InheritableThreadLocal",//
+      "InstantiationError",//
+      "InstantiationException",//
+      "Integer",//
+      "InternalError",//
+      "InterruptedException",//
+      "Iterable",//
+      "LayerInstantiationException",//
+      "LinkageError",//
+      "Long",//
+      "Math",//
+      "Module",//
+      "ModuleLayer",//
+      "NegativeArraySizeException",//
+      "NoClassDefFoundError",//
+      "NoSuchFieldError",//
+      "NoSuchFieldException",//
+      "NoSuchMethodError",//
+      "NoSuchMethodException",//
+      "NullPointerException",//
+      "Number",//
+      "NumberFormatException",//
+      "Object",//
+      "OutOfMemoryError",//
+      "Override",//
+      "Package",//
+      "Process",//
+      "ProcessBuilder",//
+      "ProcessHandle",//
+      "Readable",//
+      "Record",//
+      "ReflectiveOperationException",//
+      "Runnable",//
+      "Runtime",//
+      "RuntimeException",//
+      "RuntimePermission",//
+      "SafeVarargs",//
+      "SecurityException",//
+      "SecurityManager",//
+      "Short",//
+      "StackOverflowError",//
+      "StackTraceElement",//
+      "StackWalker",//
+      "StrictMath",//
+      "String",//
+      "StringBuffer",//
+      "StringBuilder",//
+      "SuppressWarnings",//
+      "System",//
+      "Thread",//
+      "ThreadDeath",//
+      "ThreadGroup",//
+      "ThreadLocal",//
+      "Throwable",//
+      "TypeNotPresentException",//
+      "UnknownError",//
+      "UnsatisfiedLinkError",//
+      "UnsupportedClassVersionError",//
+      "UnsupportedOperationException",//
+      "VerifyError",//
+      "VirtualMachineError",//
+      "Void"//
+  };
+
+  private final String[] javautilClasses = new String[] {
+      "AbstractCollection",//
+      "AbstractList",//
+      "AbstractMap",//
+      "AbstractQueue",//
+      "AbstractSequentialList",//
+      "AbstractSet",//
+      "ArrayDeque",//
+      "ArrayList",//
+      "Arrays",//
+      "Base64",//
+      "BitSet",//
+      "Calendar",//
+      "Collection",//
+      "Collections",//
+      "Comparator",//
+      "ConcurrentModificationException",//
+      "Currency",//
+      "Date",//
+      "Deque",//
+      "Dictionary",//
+      "DoubleSummaryStatistics",//
+      "DuplicateFormatFlagsException",//
+      "EmptyStackException",//
+      "Enumeration",//
+      "EnumMap",//
+      "EnumSet",//
+      "EventListener",//
+      "EventListenerProxy",//
+      "EventObject",//
+      "FormatFlagsConversionMismatchException",//
+      "Formattable",//
+      "Formatter",//
+      "FormatterClosedException",//
+      "GregorianCalendar",//
+      "HashMap",//
+      "HashSet",//
+      "Hashtable",//
+      "HexFormat",//
+      "IdentityHashMap",//
+      "IllegalFormatCodePointException",//
+      "IllegalFormatConversionException",//
+      "IllegalFormatException",//
+      "IllegalFormatFlagsException",//
+      "IllegalFormatPrecisionException",//
+      "IllegalFormatWidthException",//
+      "IllformedLocaleException",//
+      "InputMismatchException",//
+      "IntSummaryStatistics",//
+      "InvalidPropertiesFormatException",//
+      "Iterator",//
+      "LinkedHashMap",//
+      "LinkedHashSet",//
+      "LinkedList",//
+      "List",//
+      "ListIterator",//
+      "ListResourceBundle",//
+      "Locale",//
+      "LongSummaryStatistics",//
+      "Map",//
+      "MissingFormatArgumentException",//
+      "MissingFormatWidthException",//
+      "MissingResourceException",//
+      "NavigableMap",//
+      "NavigableSet",//
+      "NoSuchElementException",//
+      "Objects",//
+      "Observable",//
+      "Observer",//
+      "Optional",//
+      "OptionalDouble",//
+      "OptionalInt",//
+      "OptionalLong",//
+      "PrimitiveIterator",//
+      "PriorityQueue",//
+      "Properties",//
+      "PropertyPermission",//
+      "PropertyResourceBundle",//
+      "Queue",//
+      "Random",//
+      "RandomAccess",//
+      "ResourceBundle",//
+      "Scanner",//
+      "ServiceConfigurationError",//
+      "ServiceLoader",//
+      "Set",//
+      "SimpleTimeZone",//
+      "SortedMap",//
+      "SortedSet",//
+      "Spliterator",//
+      "Spliterators",//
+      "SplittableRandom",//
+      "Stack",//
+      "StringJoiner",//
+      "StringTokenizer",//
+      "Timer",//
+      "TimerTask",//
+      "TimeZone",//
+      "TooManyListenersException",//
+      "TreeMap",//
+      "TreeSet",//
+      "UnknownFormatConversionException",//
+      "UnknownFormatFlagsException",//
+      "UUID",//
+      "Vector",//
+      "WeakHashMap",//
+  };
+
   private boolean isTarget;
 
   public JavaFileVisitor(final IVConfig config, final String remoteUrl, final RevCommit commit,
@@ -62,7 +278,7 @@ public class JavaFileVisitor extends ASTVisitor {
     isTarget = true;
     returnTypeOptional.ifPresent(r -> r.accept(this));
     parameters.forEach(p -> p.accept(this));
-    if(!isTarget){
+    if (!isTarget) {
       return false;
     }
 
@@ -74,15 +290,16 @@ public class JavaFileVisitor extends ASTVisitor {
     }
 
     // アノテーションを削除
-    node.modifiers().removeIf(m -> m instanceof MarkerAnnotation || m instanceof NormalAnnotation);
+    node.modifiers()
+        .removeIf(m -> m instanceof MarkerAnnotation || m instanceof NormalAnnotation);
 
     // staticを削除
-    node.modifiers().removeIf(m -> m instanceof Modifier && ((Modifier)m).isStatic());
+    node.modifiers()
+        .removeIf(m -> m instanceof Modifier && ((Modifier) m).isStatic());
 
     // privateを削除
-    node.modifiers().removeIf(m -> m instanceof Modifier && ((Modifier)m).isPrivate());
-
-
+    node.modifiers()
+        .removeIf(m -> m instanceof Modifier && ((Modifier) m).isPrivate());
 
     // 返値，メソッド名，メソッド全体の文字列, パスを利用してメソッドオブジェクトを生成
     final String returnType = returnTypeOptional.map(ASTNode::toString)
@@ -128,7 +345,7 @@ public class JavaFileVisitor extends ASTVisitor {
 
   @Override
   public boolean visit(final ParameterizedType node) {
-    isTarget = false;
+    // 型引数についてはなにもしない
     return super.visit(node);
   }
 
@@ -148,7 +365,9 @@ public class JavaFileVisitor extends ASTVisitor {
   @Override
   public boolean visit(final SimpleType node) {
     final String typeName = node.toString();
-    if (!typeName.equals("String")) {
+    // java.langもしくはjava.utilのいずれのクラス名にも一致しない場合は対象外のクラスと見なす
+    if (!Stream.concat(Arrays.stream(javalangClasses), Arrays.stream(javautilClasses))
+        .anyMatch(c -> c.equals(typeName))) {
       isTarget = false;
     }
     return super.visit(node);
