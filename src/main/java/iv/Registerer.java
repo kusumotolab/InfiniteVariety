9d2251496a4065a8f4898d7e4937e7e283ec5cc1
Yoshiki Higo <yoshiki.higo@gmail.com>
2022-01-04 15:26:09 +0900

    processing for file or directory was added

--- src/main/java/iv/Registerer.java
+++ src/main/java/iv/Registerer.java
@@ -1,11 +1,17 @@
 package iv;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import org.eclipse.jgit.revwalk.RevCommit;
 import org.kohsuke.args4j.CmdLineException;
 import org.kohsuke.args4j.CmdLineParser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import iv.ast.JavaMethodExtractor;
 import iv.data.JavaMethod;
 import iv.db.JavaMethodDAO;
 import iv.git.GitRepo;
@@ -49,12 +55,59 @@ public class Registerer {
   public void exec() {
     logger.trace("enter exec()");
 
-    final GitRepo gitRepo = new GitRepo(config);
-    final RevCommit headCommit = gitRepo.getHeadCommit();
-    final List<JavaMethod> javaMethods = gitRepo.getJavaMethods(headCommit);
+    final List<JavaMethod> javaMethods = new ArrayList<>();
+    final Path filePath = config.getFilePath();
+    final Path repoPath = config.getRepoPath();
+    if (null == filePath && null == repoPath) {
+      System.err.println("either of \"-f\" or \"-r\" must be specified.");
+      System.exit(0);
+    } else if (null != filePath && null != repoPath) {
+      System.err.println("both of \"-f\" and \"-r\" must not be specified.");
+      System.err.println("please specify either of them.");
+      System.exit(0);
+    } else if (null != filePath) {
+      javaMethods.addAll(getJavaMethods(filePath));
+    } else if (null != repoPath) {
+      final GitRepo gitRepo = new GitRepo(config);
+      final RevCommit headCommit = gitRepo.getHeadCommit();
+      javaMethods.addAll(gitRepo.getJavaMethods(headCommit));
+    }
 
     JavaMethodDAO.SINGLETON.initialize(config);
     JavaMethodDAO.SINGLETON.addMethods(javaMethods);
     JavaMethodDAO.SINGLETON.close();
   }
+
+  private List<JavaMethod> getJavaMethods(final Path dir) {
+    final List<JavaMethod> javaMethods = new ArrayList<>();
+
+    // dirがファイルのとき
+    if (Files.isRegularFile(dir)) {
+      final String fileName = dir.toString();
+      if (fileName.endsWith(".java")) {
+        final JavaMethodExtractor extractor = new JavaMethodExtractor(config, null, null);
+        try {
+          final String text = Files.readString(dir);
+          final List<JavaMethod> methods = extractor.getJavaMethods(dir.toString(), text);
+          javaMethods.addAll(methods);
+        } catch (final IOException e) {
+          System.err.println(e.getMessage());
+        }
+      }
+    }
+
+    // dirがディレクトリのとき
+    else if (Files.isDirectory(dir)) {
+      try {
+        Files.list(dir)
+            .forEach(c -> javaMethods.addAll(getJavaMethods(c)));
+      }catch(final IOException e){
+        System.err.println(e.getMessage());
+      }
+    }
+
+    return javaMethods;
+  }
+
+
 }
