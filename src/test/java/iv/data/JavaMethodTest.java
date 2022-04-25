dfc72a7130518fcf19f22f8e92c3382301942d60
Yoshiki Higo <yoshiki.higo@gmail.com>
2022-04-25 10:48:48 +0900

    add argument

--- src/test/java/iv/data/JavaMethodTest.java
+++ src/test/java/iv/data/JavaMethodTest.java
@@ -8,21 +8,21 @@ public class JavaMethodTest {
   @Test
   public void test_isTest1() {
     final JavaMethod method = new JavaMethod("return", "name", "rawText", "normalizedText", 1,
-        "aaa/test/bbb/C.java", 1, 10, "repository", null);
+        0, "aaa/test/bbb/C.java", 1, 10, "repository", null);
     assertThat(method.isTest()).isTrue();
   }
 
   @Test
   public void test_isTest2() {
     final JavaMethod method = new JavaMethod("return", "name", "rawText", "normalizedText", 1,
-        "aaa/testData/bbb/C.java", 1, 10, "repository", null);
+        0, "aaa/testData/bbb/C.java", 1, 10, "repository", null);
     assertThat(method.isTest()).isTrue();
   }
 
   @Test
   public void test_isTest3() {
     final JavaMethod method = new JavaMethod("return", "name", "rawText", "normalizedText", 1,
-        "aaa/bbb/C.java", 1, 10, "repository", null);
+        0, "aaa/bbb/C.java", 1, 10, "repository", null);
     assertThat(method.isTest()).isFalse();
   }
 }
